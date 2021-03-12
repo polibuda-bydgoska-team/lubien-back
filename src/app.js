58f7297fff666a766638cb26ccfde7816a5b5546
Adrian <adibu8@gmail.com>
2021-03-12 18:53:43 +0100

    Create admin controller

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 require("./db/db-connection");
+const path = require("path");
 
 const adminRouter = require("./routers/admin");
 const notFoundRouter = require("./routers/404");
 const errorHandler = require("./middleware/errorHandler");
+const imagesUploader = require("./middleware/imagesUploader");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+// // app.use(imagesUploader);
+// app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/admin", adminRouter);
 app.use(notFoundRouter);
 app.use(errorHandler);
--- src/models/user.js
+++ src/models/user.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema({
     items: [
       {
         productId: {
-          type: Schema.Types.ObjectId,
+          type: mongoose.Schema.Types.ObjectId,
           ref: "Product",
           required: true,
         },
