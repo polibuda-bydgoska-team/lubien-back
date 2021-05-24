5839acf46e817f2ae6231ae0645347e2de3f6cef
Adrian <adibu8@gmail.com>
2021-05-24 14:33:42 +0200

    LUB-BACK75 Custom dashboard in AdminBro

--- .adminbro/.entry.js
+++ .adminbro/.entry.js
@@ -1,1 +1,3 @@
 AdminBro.UserComponents = {}
+import Component1 from '../src/adminBro/components/dashboard'
+AdminBro.UserComponents.Component1 = Component1
---
+++ src/adminBro/components/dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@admin-bro/design-system";
+
+const Dashboard = () => (
+  <center>
+    <Box variant="grey">
+      <img src="https://ik.imagekit.io/lubien/Branding/logo_white_XwISseNAp.png" />
+    </Box>
+  </center>
+);
+
+export default Dashboard;
